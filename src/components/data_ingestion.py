9f18cfca250d3c3f21b59a026476ecd6c78ec7da
Shima Maleki <shimamaleki95@yahoo.com>
2025-01-05 19:57:13 +0000

    data ingestion class initiating

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from pandas import DataFrame
+
+class DataIngestion:
+    def __init__(self, file_path: str): 
+        self.file_path = file_path 
+
+    def read_dataset(self):
+        df = pd.read_csv(self.file_path)
+        return df
+
+    def clean_dataset(self):
+        dataset = self.read_dataset()
+        df = dataset[dataset['Salary Estimate'] != '-1']
+        return df
+
+
+if __name__ == "__main__":
+    df_object= DataIngestion(file_path= 'data\df.csv')
+    dataset = df_object.clean_dataset()
+    print(dataset.head())
+    
+
+
+  
+
+    
+        
+  
